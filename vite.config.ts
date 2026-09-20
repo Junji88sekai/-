29705d9f3ab1a3e304557ab8380875a069a60956
Junji88sekai <junyamasaki.es@gmail.com>
2026-09-20 19:37:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/-/',
+    base: '/rakurakukatakanaoboeyo/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
